be14d63e12c59a2975307a0d20e5d9d8ef5a6f45
Mohammad Rahman <rahmanmm317@gmail.com>
2022-02-04 01:32:15 -0500

    initial commit

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz() {
+    for(var i = 1; i <= 100; i++) {
+        if(i % 3 == 0) {
+            if(i % 5 == 0) {
+                console.log('"' + "FizzBuzz" + '"');
+            }
+            console.log('"'+"Fizz"+'"')
+        } else if(i % 5 == 0) {
+            console.log('"' + "Buzz" + '"');
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+
+fizzbuzz();
